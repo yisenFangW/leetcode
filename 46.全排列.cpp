b1b7e87f0fc0a6b338eda37d1611698f607dc00a
yisenFangW <32295860+yisenFangW@users.noreply.github.com>
2019-07-02 01:05:26 +0800

    Update 46.全排列.cpp

--- 46.全排列.cpp
+++ 46.全排列.cpp
@@ -14,7 +14,7 @@
 ]
 
 题解：
-//这题首先拿到题目，就应该是要想到是用递归去做的，想到可能还有动态规划能做这样的题目。想道回溯法，但是没有实际动手去做，因为
+//1.这题首先拿到题目，就应该是要想到是用递归去做的，想到可能还有动态规划能做这样的题目。想道回溯法，但是没有实际动手去做，因为
 //不知道具体的操作是什么，了解到可以用一个visted数组去标记遍历过的和没有遍历过的，就是很明显的回溯法做法；
 void permuteDFS(vector<vector<int>> &res, vector<int>& nums, int level, vector<int> &visted, vector<int> &out) {
 	if (level == nums.size()) {
@@ -39,5 +39,25 @@ vector<vector<int>> permute(vector<int>& nums) {
 	vector<int> visted(nums.size(), 0);
 	vector<int> out;
 	permuteDFS(res, nums, 0, visted, out);
+	return res;
+}
+
+//2.更加简洁的递归写法
+void permuteDFS(vector<vector<int>> &res, vector<int>& nums, int start) {
+	if (start == nums.size()) {
+		res.push_back(nums);
+		return;
+	}
+	//跟上面解法是一样的，但是这样写起来会更加简洁，也不用标记是否用过了
+	for (int i = start; i < nums.size(); ++i) {
+		swap(nums[start], nums[i]);
+		permuteDFS(res, nums, start + 1);
+		swap(nums[start], nums[i]);
+	}
+}
+
+vector<vector<int>> permute(vector<int>& nums) {
+	vector<vector<int>> res;
+	permuteDFS(res, nums, 0);
 	return res;
 }
