d43a42c02d27689a3603ea4660ea8969e7ec3a37
yisenFangW <32295860+yisenFangW@users.noreply.github.com>
2019-07-30 23:40:26 +0800

    注意这题的解题思路
    题目不是很难，主要就是解题思路，这种题还需要回头反复来看，一眼就能知道怎么写的那种才阔以；

---
+++ 16. 最接近的三数之和.cpp
@@ -0,0 +1,33 @@
+给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+
+例如，给定数组 nums = [-1，2，1，-4], 和 target = 1.
+
+与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
+
+题解：
+//这题是找出与target最接近的三个数，首先那道题不是很清楚解题的思路，看了一下网上别人的解题方法，先将数组排序，然后确定一个数的位置
+//这个确定的数的位置，从左往右走，然后设置left和right指针，一共可以确定三个数的位置，最后记录下差值最小的一个数值；
+
+int threeSumClosest(vector<int>& nums, int target) {
+    int diff = INT_MAX;
+    int sum = INT_MAX;
+    int res = INT_MAX;
+    sort(nums.begin(), nums.end());
+    for(int i=0;i<nums.size();++i){
+        int left = i+1, right = nums.size()-1;
+        while(left < right){
+            sum = nums[i]+nums[left]+nums[right];
+            if(sum == target)
+                return target;
+            if(diff > abs(target - sum)) {
+                res = sum;
+                diff = abs(target - sum);
+            }
+            if(sum < target)
+                ++left;
+            else
+                --right;
+        }
+    }
+    return res;
+}
