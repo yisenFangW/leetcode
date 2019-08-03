795c36b34f414f25d9d6f8e7d774b6aa4e2190d0
yisenFangW <32295860+yisenFangW@users.noreply.github.com>
2019-08-04 00:01:24 +0800

    添加网上另一种解法

--- 146. LRU缓存机制.cpp
+++ 146. LRU缓存机制.cpp
@@ -71,5 +71,34 @@ private:
 
 
 //2.看的别人的题解，用list与unordered_map实现，并不好理解，这里作为记录一下；
-        
+class LRUCache{
+public:
+    LRUCache(int capacity) {
+        cap = capacity;
+    }
+    
+    int get(int key) {
+        auto it = m.find(key);
+        if (it == m.end()) return -1;
+        l.splice(l.begin(), l, it->second);
+        return it->second->second;
+    }
+    
+    void put(int key, int value) {
+        auto it = m.find(key);
+        if (it != m.end()) l.erase(it->second);
+        l.push_front(make_pair(key, value));
+        m[key] = l.begin();
+        if (m.size() > cap) {
+            int k = l.rbegin()->first;
+            l.pop_back();
+            m.erase(k);
+        }
+    }
+    
+private:
+    int cap;
+    list<pair<int, int>> l;
+    unordered_map<int, list<pair<int, int>>::iterator> m;
+};    
         
